04d1cc4993f30bc648f86d86a50e363adfd38a6c
Danverde <gre12014@byui.edu>
2018-03-01 14:56:37 -0700

    added fileId's to students on drifter

--- drifter.js
+++ drifter.js
@@ -15,28 +15,64 @@ var drifter = {
             }
         },
         bob: {
-            id: "34729",
+            id: "34729"
         },
         charli: {
             id: "34730",
+            files: {
+                docx: 441469,
+                pdf: 441466,
+                xlsx: 441467,
+                plainText: 441468
+            }
         },
         david: {
             id: "34731",
+            files: {
+                docx: 441474,
+                pdf: 441471,
+                xlsx: 441472,
+                plainText: 441473
+            }
         },
         eugene: {
             id: "34766",
+            files: {
+                docx: 441478,
+                pdf: 441475,
+                xlsx: 441476,
+                plainText: 441477
+            }
         },
         faith: {
             id: "34767",
         },
         guy: {
             id: "34768",
+            files: {
+                docx: 441482,
+                pdf: 441479,
+                xlsx: 441480,
+                plainText: 441481
+            }
         },
         hope: {
             id: "34769",
+            files: {
+                docx: 441486,
+                pdf: 441483,
+                xlsx: 441484,
+                plainText: 441485
+            }
         },
         ima: {
             id: "34770",
+            files: {
+                docx: 441490,
+                pdf: 441487,
+                xlsx: 441488,
+                plainText: 441489
+            }
         },
     },
     admin: { // Can this pull from local auth.json?
