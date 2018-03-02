efff5d35689dabd14e1d7f6d897864f244a8b7ca
Danverde <gre12014@byui.edu>
2018-03-01 17:31:25 -0700

    cleaned assignmentSubmissions

--- assignmentSubmissions.js
+++ assignmentSubmissions.js
@@ -22,17 +22,11 @@ function submitAssignmentById(assignmentId, courseId, studentId, fileId, cb) {
 }
 
 function submitAssignmentText(assignmentId, courseId, studentId, text, cb) {
-    var url = `/api/v1/courses/${courseId}/assignments/${assignmentId}/submissions/?as_user_id=${studentId}`;
-    /* postObj = {
-        submission: {
-            submission_type: 'online_text_entry',
-            body: text
-        }
-    }; */
-    postObj = {
-        "submission[submission_type]": 'online_text_entry',
-        "submission[body]": text
-    }
+    var url = `/api/v1/courses/${courseId}/assignments/${assignmentId}/submissions/?as_user_id=${studentId}`,
+        postObj = {
+            "submission[submission_type]": 'online_text_entry',
+            "submission[body]": text
+        };
     submitAssignment(url, postObj, cb);
 }
 
@@ -43,7 +37,7 @@ function submitAssigmentURL(assignmentId, courseId, studentId, postUrl, cb) {
             "submission[url]": postUrl
         };
 
-    submitAssignment(url, postObj, cb)
+    submitAssignment(url, postObj, cb);
 }
 
 module.exports = {
@@ -53,8 +47,8 @@ module.exports = {
 
 // FOR TESTING
 const studentId = 34728;
-var textToSubmit = '<html><body><h1>I worked very hard on this. I deserve an A.</h1></body></html>';
-/* submitAssignmentText(95014, 80, studentId, textToSubmit, (err, submission) => {
+/*var textToSubmit = '<html><body><h1>I worked very hard on this. I deserve an A.</h1></body></html>';
+submitAssignmentText(95014, 80, studentId, textToSubmit, (err, submission) => {
     if (err) console.error(err);
     else console.log(submission);
 }); */
@@ -63,7 +57,7 @@ var urlToSubmit = 'https://www.google.com';
 submitAssigmentURL(95015, 80, studentId, urlToSubmit, (err, stuff) => {
     if (err) console.error(err);
     else console.log(stuff);
-})
+});
 
 
 /* submitAssignmentById(92269, 80, studentId, 380876, (err, submission) => {
