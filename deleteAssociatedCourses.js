25dbbdd9522a39a5486bc726956b089d8cbcab07
Joshua McKinney <mckinneyj@byui.edu>
2018-03-14 16:08:59 -0600

    it makes courses a bit loger

--- deleteAssociatedCourses.js
+++ deleteAssociatedCourses.js
@@ -1,14 +1,32 @@
 const canvas = require('canvas-wrapper');
-const masterCourse = 4274;
+const asyncLib = require('async');
+const masterCourse = 4272;
 
-canvas.get(`/api/v1/courses/${masterCourse}/blueprint_templates/default/associated_courses`, (err, courses) => {
-    courses.forEach(course => {
-        canvas.delete(`/api/v1/courses/${course.id}?event=delete`, (err) => {
+function deleteCourses(courses) {
+    asyncLib.eachLimit(courses, 10, (courcourse, cb) => {
+        canvas.delete(`/api/v1/courses/${courcourse.id}?event=delete`, (err) => {
             if (err) {
                 console.log(err);
+                cb(err);
                 return;
             }
-            console.log(`Course Deleted: ${course.name}`);
+            console.log(`Course Deleted: ${courcourse.name} with id:${courcourse.id}`);
+            cb();
         });
     });
-});
+}
+
+// canvas.get(`/api/v1/courses/${masterCourse}/blueprint_templates/default/associated_courses`, (err, courses) => {
+//     deleteCourses(courses);
+// });
+
+var byeBye = [7792, 8012, 8013, 8019, 8034]
+    .map((num, i) => {
+        return {
+            id: num,
+            name: i + 1
+        };
+    });
+    
+console.log(byeBye);
+deleteCourses(byeBye);
--- identifyInstructorsFromCSV.js
+++ identifyInstructorsFromCSV.js
@@ -5,24 +5,31 @@ const moment = require('moment');
 const d3dsv = require('d3-dsv');
 
 //remove the BOM off the front of the file
-var csv = fs.readFileSync('./InstructorList.csv', 'utf8').replace(/^\uFEFF/, '');;
+var csv = fs.readFileSync('./InstructorList.csv', 'utf8').replace(/^\uFEFF/, '');
 
 var badPeople = [];
 
 
 
-var csvData = d3dsv.tsvParse(csv, (d,i) => {
+var csvData = d3dsv.tsvParse(csv, (d, i) => {
     return {
         lastname: d.lastname,
         middlename: d.middlename,
         firstname: d.firstname,
         username: d.username,
-        Inumber: d.INumber.replace(/-/g, "").padStart(9,'0'),
+        Inumber: d.INumber.replace(/-/g, "").padStart(9, '0'),
         email: d.email,
     };
 });
 
 console.log(csvData.length);
+var dups = csvData.map(guy => `${guy.firstname} ${guy.lastname}`).sort().filter((guy, i, arr) => {
+    return i < arr.length && arr[i + 1] === guy;
+});
+
+console.log("FYI these names have at least 2 people that have the same name");
+console.log(dups);
+
 asyncLib.mapLimit(csvData, 25, (instructor, callback) => {
 
     canvas.get(`/api/v1/accounts/1/users?search_term=${instructor.Inumber}`, (err, userArr) => {
@@ -33,10 +40,10 @@ asyncLib.mapLimit(csvData, 25, (instructor, callback) => {
         }
         // if (userArr.length > 0 && instructor.username.toLocaleLowerCase() === userArr[0].login_id.toLocaleLowerCase()) {
         if (userArr.length > 0 && instructor.Inumber === userArr[0].sis_user_id) {
-        callback(null, userArr[0]);
+            callback(null, userArr[0]);
         } else {
-            console.log("CSV=", JSON.stringify(instructor,null,4));
-            console.log("Canvas", JSON.stringify(userArr[0],null,4));
+            console.log("CSV=", JSON.stringify(instructor, null, 4));
+            console.log("Canvas", JSON.stringify(userArr[0], null, 4));
             console.log('---------------------------------');
             badPeople.push(instructor);
             callback(null, null);
