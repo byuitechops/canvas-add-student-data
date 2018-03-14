8ab69b4efbffa04e432aadd0068ae94d933e94c3
Joshua McKinney <mckinneyj@byui.edu>
2018-03-14 00:52:58 -0600

    fixed identifyInstrors for BOM, inumbers with dashes and nonleading 0s

--- identifyInstructorsFromCSV.js
+++ identifyInstructorsFromCSV.js
@@ -2,29 +2,27 @@ const fs = require('fs');
 const canvas = require('canvas-wrapper');
 const asyncLib = require('async');
 const moment = require('moment');
+const d3dsv = require('d3-dsv');
 
-var csv = fs.readFileSync('./InstructorList.csv').toString();
+//remove the BOM off the front of the file
+var csv = fs.readFileSync('./InstructorList.csv', 'utf8').replace(/^\uFEFF/, '');;
 
 var badPeople = [];
 
-var csvData = csv.split('\r');
 
-csvData = csvData.map(person => {
-    var details = person.split('\t');
 
+var csvData = d3dsv.tsvParse(csv, (d,i) => {
     return {
-        lastname: details[0],
-        middlename: details[1],
-        firstname: details[2],
-        username: details[3],
-        Inumber: details[4],
-        email: details[5],
+        lastname: d.lastname,
+        middlename: d.middlename,
+        firstname: d.firstname,
+        username: d.username,
+        Inumber: d.INumber.replace(/-/g, "").padStart(9,'0'),
+        email: d.email,
     };
 });
 
 console.log(csvData.length);
-
-
 asyncLib.mapLimit(csvData, 25, (instructor, callback) => {
 
     canvas.get(`/api/v1/accounts/1/users?search_term=${instructor.Inumber}`, (err, userArr) => {
@@ -33,9 +31,13 @@ asyncLib.mapLimit(csvData, 25, (instructor, callback) => {
             badPeople.push(instructor);
             callback(null, null);
         }
-        if (userArr.length > 0 /*&& instructor.username === userArr[0].login_id*/) {
-            callback(null, userArr[0]);
+        // if (userArr.length > 0 && instructor.username.toLocaleLowerCase() === userArr[0].login_id.toLocaleLowerCase()) {
+        if (userArr.length > 0 && instructor.Inumber === userArr[0].sis_user_id) {
+        callback(null, userArr[0]);
         } else {
+            console.log("CSV=", JSON.stringify(instructor,null,4));
+            console.log("Canvas", JSON.stringify(userArr[0],null,4));
+            console.log('---------------------------------');
             badPeople.push(instructor);
             callback(null, null);
         }
@@ -48,9 +50,9 @@ asyncLib.mapLimit(csvData, 25, (instructor, callback) => {
         peopleObjects = peopleObjects.filter(item => item !== null);
         console.log('Found Users:', peopleObjects.length);
         console.log('Bad Users:', badPeople.length);
-        fs.writeFileSync(`./UserData-(${moment().format('d-M H-m')}).json`, JSON.stringify(peopleObjects, null, '\t'));
+        fs.writeFileSync(`./UserData-(${moment().format('d-M H-m')}).json`, JSON.stringify(peopleObjects, null, 4));
         if (badPeople.length > 0) {
-            fs.writeFileSync(`./BadUsers-(${moment().format('d-M H-m')}).json`, JSON.stringify(badPeople, null, '\t'));
+            fs.writeFileSync(`./BadUsers-(${moment().format('d-M H-m')}).json`, JSON.stringify(badPeople, null, 4));
         }
     }
 });
