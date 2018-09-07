326bc3322c993ced7e96b20661ce669c74c4182a
Joshua McKinney <mckinneyj@byui.edu>
2018-09-07 14:26:16 -0600

    email, name, and inumber or canvasid are required now

--- identifyInstructorsFromCSV.js
+++ identifyInstructorsFromCSV.js
@@ -4,40 +4,100 @@ const asyncLib = require('async');
 const moment = require('moment');
 const d3dsv = require('d3-dsv');
 
+
+/***************************** the file name needs to change */
 //remove the BOM off the front of the file
-var csv = fs.readFileSync('./chad.csv', 'utf8').replace(/^\uFEFF/, '');
+var csv = fs.readFileSync('./jilaneStudents.csv', 'utf8').replace(/^\uFEFF/, '');
+/*********************************************************** */
 
-var badPeople = [];
+var badPeople = [],
+    missingIs = [],
+    missingEmail = [],
+    noEmailInSystem = [];
 
-function getKey(obj, key){
-    if(typeof obj[key] !== 'undefined'){
-        return obj[key].trim();
-    } else {
-        return '';
+
+function fixInumber(number) {
+    if(number === undefined){
+        return null;
     }
+
+    try {
+        number = trimOrBlank(number);
+        //remove spaces, dashes and pad front with 0s
+        var numberOut = number
+            .replace(/ /g, "")
+            .replace(/-/g, "")
+            .padStart(9, '0');
+
+        var checkIs9Numbers = /\d{9}/;
+        if (numberOut === '000000000' || !checkIs9Numbers.test(numberOut)) {
+            return null;
+        }
+    } catch (e) {
+        console.log(number);
+        console.log(numberOut);
+        console.log(e.message);
+        throw new Error("Something wrong with iNumber");
+    }
+    return numberOut;
 }
 
-function fixInumber(number){
-    if(number === ''){
-        return number;
-    }
-    return number.replace(/-/g, "").padStart(9, '0');
+function fixEmail(email) {
+    email = trimOrBlank(email);
+    var checkIsStudnetEmail = /([a-z]+|[a-z]{3}\d+)@byui.edu/i,
+        checkIsByuiEmail = /[a-z0-9]+@byui.edu/i;
 
+    //some emails are not real
+    if ((email === '@byui.edu' || !(checkIsStudnetEmail.test(email))) && checkIsByuiEmail.test(email)) {
+        return null;
+    }
+    return email.toLowerCase();
 }
 
-var csvData = d3dsv.csvParse(csv, (d, i) => {
+function fixName(nameIn) {
+    return nameIn.split(' ')
+        //Proper Case the name field 
+        .map(namePart => {
+            //shouldn't need trim because of split on space but just in case, 
+            namePart = namePart.trim();
+            //lowerCase everything
+            namePart = namePart.toLowerCase();
+            //upper the first
+            namePart = namePart[0].toUpperCase() + namePart.slice(1);
+
+            return namePart;
+        }).join(' ');//put all the parts back together
+}
+
+function fixCanvasId(canvasId) {
+    //if we don't have one then set it to null
+    if (canvasId === undefined) {
+        return null;
+
+    }
+    return canvasId.trim();
+}
+
+function trimOrBlank(string) {
+    if (string) {
+        return string.trim();
+    }
+    return "";
+}
+
+var csvData = d3dsv.csvParse(csv, (d) => {
+
     return {
-        lastname: getKey(d,"lastname"),
-        middlename: getKey(d,"middlename"),
-        firstname: getKey(d,"firstname"),
-        username: getKey(d,"username"),
-        Inumber: fixInumber(getKey(d,"INumber")),
-        email: getKey(d,"email"),
-        canvasUserID: +getKey(d,"canvasUserID")
+        name: fixName(d.name),
+        iNumber: fixInumber(d.iNumber),
+        email: fixEmail(d.email),
+        canvasId: fixCanvasId(d.canvasId)
     };
 });
 
-var dups = csvData.map(guy => `${guy.firstname} ${guy.lastname}`).sort().filter((guy, i, arr) => {
+//check for dups
+//right now it does it by name we should also check on all the other fields too.
+var dups = csvData.map(guy => guy.name).sort().filter((guy, i, arr) => {
     return i < arr.length && arr[i + 1] === guy;
 });
 if (dups.length > 0) {
@@ -49,59 +109,86 @@ if (dups.length > 0) {
 
 
 function searchCanvas(instructor, callback) {
-    var searchTerm = instructor.firstname + " " + instructor.lastname;
-    canvas.get(`/api/v1/accounts/1/users?search_term=${searchTerm}`, function (err, userArr) {
+    var apiCall;
+
+    function missingInumberOrCanvasId() {
+        console.log('Missing iNumber and canvasID for', instructor.name);
+        missingIs.push(instructor);
+        callback(null, null);
+    }
+
+    //see if we have what we need
+    //if no iNumber and no canvasId
+    if (instructor.iNumber === null && instructor.canvasId === null) {
+        missingInumberOrCanvasId();
+        return;
+    }
+
+    //if no email
+    if (instructor.email === null) {
+        console.log('Missing email for', instructor.name);
+        missingEmail.push(instructor);
+        callback(null, null);
+        return;
+    }
+
+    //use iNumber if you have it else use canvasId
+    if (instructor.iNumber !== null) {
+        //add on include email so we get that too
+        apiCall = `/api/v1/accounts/1/users?search_term=${instructor.iNumber}&include[]=email`;
+    } else if (instructor.canvasId !== null) {
+        apiCall = `/api/v1/users/${instructor.canvasId}`;
+    } else {
+        missingInumberOrCanvasId();
+        return;
+    }
 
 
-        var checkFunction,
-            matchingUser;
 
-        if (err) {
-            console.log('ERROR', err);
-            badPeople.push(instructor);
-            callback(null, null);
-        }
 
-        function listBad() {
+    // search by iNumber
+    canvas.get(apiCall, function (err, userArr) {
+
+        var matchingUser;
+
+        function listBad(e) {
+            if (e) {
+                console.log('ERROR', e);
+            }
             console.log("CSV=", JSON.stringify(instructor, null, 4));
-            console.log("Canvas first user in list", JSON.stringify(userArr[0], null, 4));
+            if (userArr && userArr[0]) {
+                console.log("Canvas first user in list", JSON.stringify(userArr[0], null, 4));
+            }
             console.log('---------------------------------');
             badPeople.push(instructor);
             callback(null, null);
         }
 
-        function checkUsername(csvUser, canvasUser) {
-            return csvUser.username.toLocaleLowerCase() === canvasUser.login_id.toLocaleLowerCase();
+        //just check if something exploded 
+        if (err) {
+            listBad(err);
+            return;
         }
 
-        function checkInumber(csvUser, canvasUser) {
-            return csvUser.Inumber === canvasUser.sis_user_id;
+        function checkName(csvUser, canvasUser) {
+            return csvUser.name === canvasUser.name;
         }
-        function checkUserId(csvUser, canvasUser){
-            return csvUser.canvasUserID === canvasUser.id;
+        function checkEmail(instructor, canvasUser) {
+            return instructor.email.toLowerCase() === canvasUser.email.toLowerCase();
         }
 
-
+        //if we didn't get any back from the api call
         if (userArr.length === 0) {
             listBad();
             return;
         }
 
-        //get the check function
-        if (instructor.Inumber && instructor.Inumber !== '') {
-            checkFunction = checkInumber;
-        } else if (instructor.canvasUserID !== 0) {
-            checkFunction = checkUserId;
-        } else if(instructor.username !== '' ){
-            checkFunction = checkUsername;
-        }
-
-        // get matching user
+        //See if we have someone that has a matching name or matching email
         matchingUser = userArr.find(function (canvasUser) {
-            return checkFunction(instructor, canvasUser);
+            return checkName(instructor, canvasUser) || checkEmail(instructor, canvasUser);
         });
 
-        //check if we got one
+        //if we didn't get one that matched on name or email its bad
         if (typeof matchingUser === "undefined") {
             listBad();
             return;
@@ -112,8 +199,15 @@ function searchCanvas(instructor, callback) {
     });
 }
 
-function mapCallBack(err, peopleObjects) {
+function csvMaker(array) {
+    return d3dsv.csvFormat(array);
+}
 
+function jsonMaker(array) {
+    return JSON.stringify(array, null, 4);
+}
+
+function mapCallBack(err, peopleObjects) {
     if (err) {
         console.log(err);
     } else {
@@ -121,9 +215,23 @@ function mapCallBack(err, peopleObjects) {
         peopleObjects = peopleObjects.filter(item => item !== null);
         console.log('Found Users:', peopleObjects.length);
         console.log('Bad Users:', badPeople.length);
-        fs.writeFileSync(`./UserData-(${moment().format('d-M H-m')}).json`, JSON.stringify(peopleObjects, null, 4));
+        var date = moment().format('D-MMM-YY h-m-sa');
+        var ending = '.json';
+        var processor = jsonMaker;
+
+        fs.writeFileSync(`./${date}_dateUserData.json`, JSON.stringify(peopleObjects, null, 4));
         if (badPeople.length > 0) {
-            fs.writeFileSync(`./BadUsers-(${moment().format('d-M H-m')}).json`, JSON.stringify(badPeople, null, 4));
+            var makeCSV = true;
+
+            if (makeCSV) {
+                ending = '.csv';
+                processor = csvMaker;
+            }
+
+            fs.writeFileSync(`./${date}_BadUsers${ending}`, processor(badPeople));
+            fs.writeFileSync(`./${date}_MissingI${ending}`, processor(missingIs));
+            fs.writeFileSync(`./${date}_MissingE${ending}`, processor(missingEmail));
+            fs.writeFileSync(`./${date}_MissingEInSystem${ending}`, processor(noEmailInSystem));
         }
     }
 }
