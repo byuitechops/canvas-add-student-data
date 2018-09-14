4709d8c6c44186771fa2cea51f165684645f256c
Aaron Shiffler <ajshiffler@gmail.com>
2018-09-14 15:35:45 -0600

    Added some functions to make questions work how we want

--- bin/prompts.js
+++ bin/prompts.js
@@ -5,6 +5,7 @@ console.log('Please wait...');
 const fs = require('fs');
 const path = require('path');
 var runningDir = path.resolve('./');
+var extensions = [];
 
 /*************************************************************************
  * @param {array} extension 
@@ -19,24 +20,24 @@ var filterByFileType = (extension) => {
         }
         return acc;
     }, []);
+    filteredFiles.sort((a, b) => {
+        // The larger number is later in the future
+        var aTime = fs.Stats(path.join(runningDir, a)).mtimeMs;
+        var bTime = fs.Stats(path.join(runningDir, b)).mtimeMs;
+        // This will put the larger number at the beginning
+        return bTime - aTime;
+    });
     return filteredFiles;
 };
 
 /*************************************************************************
- * @param {string} promptKey takes a string key of what prompt to change
- * @param {array} choices takes an array of choices to sort through
- * @param {function} filterFunction signature: (input, choice), returns bool
+ * This is used in assigning the source attribute for checkbox-plus
+ * Use like so:
+ * (answersSoFar, input) => cbpSourceFn(answersSoFar, input, ['array']);
+ * @param {Object} answersSoFar
+ * @param {string} input
+ * @param {array} choices
  *************************************************************************/
-var cbpSetSourceAttrib = (promptKey, choices, filterFunction) => {
-    promptsJSON[promptKey].source = (answersSoFar, input) => {
-        var filteredChoices = choices.reduce((acc, choice) => {
-            if (filterFunction(input, choice)) acc.push(choice);
-            return acc;
-        }, []);
-        return Promise.resolve(filteredChoices);
-    };
-};
-
 var cbpSourceFn = (answersSoFar, input, choices) => {
     var filteredChoices = choices.reduce((acc, choice) => {
         if (choice.includes(input)) acc.push(choice);
@@ -46,33 +47,61 @@ var cbpSourceFn = (answersSoFar, input, choices) => {
 };
 
 /*************************************************************************
- * updates the parameters that take functions with the functions with 
- * corresponding names
+ * Requires and Variables
  *************************************************************************/
-var updatePromptsJSON = () => {
-    var promptsArr = Object.keys(promptsJSON).forEach((prompt) => {
-        if (promptsJSON[prompt].type === 'checkbox-plus') {
-            promptsJSON[prompt].highlight = true;
-            promptsJSON[prompt].searchable = true;
-            promptsJSON[prompt].source = (answersSoFar, input) => cbpSourceFn(answersSoFar, input, ['Error: No Choices Specified']);
-        }
-    });
+// TODO finish this function: Find a way to get the answersSoFar into this function. This signature may or may not work
+var stepIsChosen = (answersSoFar, acceptableSteps) => {
+    if (acceptableSteps.some(answersSoFar.chooseStep)) {
+        return true;
+    }
+    return false;
 };
 
-var questions = {
+
+/*************************************************************************
+ * prompts object and vars for prompts object
+ *************************************************************************/
+var steps = {
+    step1: "Identify Instructors From CSV",
+    step2: "Build Course/Student Functions",
+    step3: "Enroll Teacher"
+};
+
+var number = {
+    [steps.step1]: 1,
+    [steps.step2]: 2,
+    [steps.step3]: 3
+};
+
+var prompts = {
+    chooseStep: {
+        type: "rawlist",
+        name: "chooseStep",
+        message: "Select which step you'd like to run:",
+        choices: [
+            steps.step1,
+            steps.step2,
+            steps.step3
+        ],
+        filter: (answersSoFar) => {
+            return number[answersSoFar.chooseStep];
+        },
+    },
     selectFile: {
         type: "checkbox-plus",
         name: "selectFile",
         message: "Select a file:",
         highlight: true,
         searchable: true,
-        source: (answersSoFar, input) => cbpSourceFn(answersSoFar, input, )
+        source: (answersSoFar, input) => cbpSourceFn(answersSoFar, input, filterByFileType(extensions)),
+        when: (answersSoFar) => stepIsChosen(answersSoFar, [1,2,3]) // Find out if this works by testing
     },
     setSandboxNumber: {
         type: "number",
         name: "setSandboxNumber",
         message: "Enter the target sandbox's sub-account number (default: 8):",
-        default: 8
+        default: 8,
+        when: stepIsChosen([2])
     },
     setMasterCourse: {
         type: "list",
@@ -83,18 +112,26 @@ var questions = {
             "Campus",
             "Other"
         ],
-        filter: "setMasterCourseNumber"
+        filter: "setMasterCourseNumber",
+        when: stepIsChosen([2])
     },
     theOtherPrompt: {
         type: "input",
         name: "theOtherPrompt",
-        message: "other:"
+        message: "other:",
+        when: stepIsChosen([2])
     },
     syncingComment: {
         type: "input",
         name: "syncingComment",
-        message: "Enter a syncing comment for canvas:"
+        message: "Enter a syncing comment for canvas:",
+        when: stepIsChosen([2])
+    },
+    runAgain: {
+        type: "confirm",
+        name: "runAgain",
+        message: "Would you like to run the program again?"
     }
 };
 
-module.exports = questions;
+module.exports = prompts;
