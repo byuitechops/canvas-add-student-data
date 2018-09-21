c2105d599fa52ef44856ed228e417020a90da768
Aaron Shiffler <ajshiffler@gmail.com>
2018-09-21 17:50:10 -0600

    Added TODO comments. Right now there is no logic other than a generic module.exports function

---
+++ brickBuildCourseFromBlueprint/deleteFailedCourse.js
@@ -0,0 +1,9 @@
+/*************************************************************************
+ * TODO Implement code that takes an object and knows which course to delete. 
+ * In buildCourseController.js, this script should be called at the
+ * appropriate time.
+ *************************************************************************/
+module.exports = (failedCourseObject) => {
+    console.log('I am deleting the course you gave me!');
+    console.log(failedCourseObject);
+};
---
+++ brickBuildCourseFromBlueprint/syncCourses.js
@@ -0,0 +1,10 @@
+/*************************************************************************
+ * TODO Abstract Syncing Information from buildCourse.js to this script.
+ * buildCourse.js currently both builds and syncs course data to canvas
+ * for better maintainability, that set of instructions should be moved
+ * here, and be called upon by the buildCourseController.js
+ *************************************************************************/
+module.exports = (couseObjectToSync) => {
+    console.log('Syncing created course to Canvas...');
+    console.log(couseObjectToSync);
+};
