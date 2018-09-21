781e8ff6e58668e86d6d6fec4d2f7593b95044df
Aaron Shiffler <ajshiffler@gmail.com>
2018-09-21 17:49:09 -0600

    wrapped whole thing in a module.export and plugged in master variables where they were needed.

--- brickBuildCourseFromBlueprint/buildCourseController.js
+++ brickBuildCourseFromBlueprint/buildCourseController.js
@@ -3,3 +3,4 @@ const buildCourses = require( path.join(__dirname, 'buildCourse.js') );
 const studentFunctions = require( path.join(__dirname, 'studentFunctions.js') );
 const syncCourses = require( path.join(__dirname, 'syncCourses.js') );
 const deleteCourses = require( path.join(__dirname, 'deleteFailedCourses.js') );
+
